56a0fb413b72c539ccd7ba1acaeffcb0a011737a
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-06-23 16:16:07 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using Puppet, install flask
-packages { 'install python packages':
+package { 'install python packages':
   command   => 'pip3 install flask flask_restful apiai',
   ensure          =>  'installed',
   install_options =>  ['-v', '2.1.0'],

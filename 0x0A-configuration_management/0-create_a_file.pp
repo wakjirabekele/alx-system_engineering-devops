1843b062971bcb6702df294890d87418ffb0c2f9
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-06-23 16:03:41 +0300

    my commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, create a file in /tmp
+file { '/tmp/school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}

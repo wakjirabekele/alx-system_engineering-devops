86b2a85c9bd704c7ce14bf0fef1d437080d955b4
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-08-09 14:00:32 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# incorrect file name
-exec{ 'fix-wordpress':
-    command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/:/bin/',
+# Fourth web-stack debug script in puppet
+exec { 'wsd3':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    =>  ['/bin', '/usr/bin']
 }
